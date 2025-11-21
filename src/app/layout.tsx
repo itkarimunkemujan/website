234a6bb896d3e21acabe27b3bf9107d5dd5ef6b1
Andre <andree.vortex@gmail.com>
2025-11-21 21:30:37 +0700

    fix: change the default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className={`${bricolage.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
